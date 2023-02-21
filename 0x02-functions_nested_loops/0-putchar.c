4df4e0516658ac18e4b5e2057edb88299c76bdd7
kabandapatrick2580 <sendsome2019@gmail.com>
2023-02-21 10:15:28 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+/**
+  * main - Entry point
+  *
+  * Return: always 0 (succes)
+  * On error, -1 is returned, and errno is set appropriately.
+  */
+int main(void)
+{
+	putchar(_putchar());
+	return (0);
+}
