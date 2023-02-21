184c5e33f75dd49ba2a5234869bb7d5c34bc87a8
kabandapatrick2580 <sendsome2019@gmail.com>
2023-02-21 12:40:19 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - CHeck code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char b;
+
+	for (b = 'a'; b <= 'z'; z++)
+	{
+		putchar('b');
+	}
+	putchar('\n');
+	return (0);
+}
