6af47719119c3d536ebb087617f278cbd398016a
kabandapatrick2580 <sendsome2019@gmail.com>
2023-02-21 21:27:21 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_sign - function
+ * @n: a character to print signs
+ * Return: 1,0 other wise -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+		return (-1);
+}
+
