e37d9a02e84e7775a3fc25d690d81309175f386d
kabandapatrick2580 <sendsome2019@gmail.com>
2023-02-21 22:13:48 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Absolute function
+ * @t: the character
+ * Return: 0 always (success)
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+	{
+		return (t);
+	}
+	else
+		return (-t);
+}
