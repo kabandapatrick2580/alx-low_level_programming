a44cd169e5265ab2460d0c1a83ee399723c4153e
kabandapatrick2580 <sendsome2019@gmail.com>
2023-02-22 12:41:24 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adding two inintegers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: return sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
