cff025bc968061e8192f48f389e670017623f332
kabandapatrick2580 <sendsome2019@gmail.com>
2023-03-03 09:05:06 -0800

    Revert "4-rev_array.c"
    This reverts commit f850b2cd5901bfe13d200a6aa40060459ad3bfff.

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++
@@ -1,5 +0,0 @@
-#include "main.h"
-
-/**
- * print_diagonal - Enrty code
- * return: 
--- 0x04-more_functions_nested_loops/8-print_square.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-/**
- * print_square _ print symbols
- * Return: 0 always (success)
- */
-void print_square(int size)
-{
-
