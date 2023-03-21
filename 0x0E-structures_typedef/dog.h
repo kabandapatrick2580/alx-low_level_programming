1714447208041235f67e8f7078208bece75145da
kabandapatrick2580 <sendsome2019@gmail.com>
2023-03-21 12:22:05 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - short description
+ *@name : first member
+ *@age : second member
+ *@owner : third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
