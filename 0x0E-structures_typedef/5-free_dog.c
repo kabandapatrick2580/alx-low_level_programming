5f02593b919fede8f5465c3d6f0b8c1c365c7eda
kabandapatrick2580 <sendsome2019@gmail.com>
2023-03-21 12:56:18 -0700

    Multiple structures

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - function thet frees dog
+ * @d: dog to be freed
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+		free(d->name);
+		free(d->owner);
+		free(d);
+}
