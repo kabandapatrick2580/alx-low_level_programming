edb09fd67af91d043e3e11d2a5fa8d13e42a4b53
kabandapatrick2580 <sendsome2019@gmail.com>
2023-03-31 08:01:47 -0700

    another commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,27 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct node - singly linked list
+ * @str: string
+ * @len: length off string
+ * @next: points to the next node
+ */
+typedef struct node
+{
+	char *str;
+	unsigned int len;
+	struct node *next;
+} list_t;
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
 #endif
