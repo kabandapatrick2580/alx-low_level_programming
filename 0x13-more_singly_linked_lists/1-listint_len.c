4964fcf51b3241b5300b3e520ba17219dea3e5de
kabandapatrick2580 <sendsome2019@gmail.com>
2023-04-05 00:52:14 -0700

    list_olen

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements
+ * @h: head of node
+ *
+ * Return: number of elements
+ */
+size_t listint_len(const listint_int *h)
+{
+	size_t node_counter = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		node_counter++
+	}
+	return (node_counter);
+}
